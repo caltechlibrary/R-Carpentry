0701d197f50e01228e3eb2cfc5319f31bfb409db
dtwrub <dtwrub@caltech.edu>
2019-03-07 15:41:48 -0800

    updates to 01, setup

--- 2019-03/setup.R
+++ 2019-03/setup.R
@@ -1,28 +1,4 @@
-## file structure
-if (! file.exists("data")) dir.create("data")
-
-if (!file.exists("data/surveys.csv")) {
-    download.file("https://ndownloader.figshare.com/files/2292172",
-                  "data/surveys.csv")
-}
-if (!file.exists("data/species.csv")) {
-    download.file("https://ndownloader.figshare.com/files/3299483",
-                  "data/species.csv")
-}
-if (!file.exists("data/plots.csv")) {
-    download.file("https://ndownloader.figshare.com/files/3299474",
-                  "data/plots.csv")
-}
-if (!file.exists("data/portal_data_joined.csv")) {
-    download.file("https://ndownloader.figshare.com/files/2292169",
-                  "data/portal_data_joined.csv")
-}
-
-if (!file.exists("data/portal_mammals.sqlite")) {
-    download.file("https://ndownloader.figshare.com/files/2292171",
-                  "data/portal_mammals.sqlite")
-}
-
+## file structure should be from zip file
 
 ## knitr options
 library(knitr)
