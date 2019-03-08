aec1e0975fce95afe27f5d20d2e31cf20000446b
dtwrub <dtwrub@caltech.edu>
2019-03-08 14:04:56 -0800

    Updates on scripts and 01

---
+++ 2019-03/load_packages.R
@@ -0,0 +1,13 @@
+# Load needed packages into R environment
+
+library(gridExtra) # ggplot
+library(hexbin) # ggplot
+library(tidyverse) # lesson 3 onwards
+library(lubridate)
+library(readr)
+library(ggplot2)
+library(dplyr)
+library(magrittr)
+library(tidyr)
+library(knitr)
+library(methods)
