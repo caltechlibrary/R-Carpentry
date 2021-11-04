dfdaa0ecafe4019a0367d9894eb486f43f76df3f
Donna Wrublewski <dtwrub@caltech.edu>
2021-11-04 08:59:56 -0700

    updated install and load packages

--- 2021-11/install_packages.R
+++ 2021-11/install_packages.R
@@ -2,17 +2,5 @@
 
 # Download needed packages
 
-# Creating R Markdown documents requires updated versions of the 
-# following packages: base64enc, digest, evaluate, glue, highr, htmltools, 
-# jquerylib, jsonlite, knitr, magrittr, markdown, mime, rmarkdown, stringi, 
-# stringr, tinytex, xfun, yaml. 
-
 install.packages("rmarkdown")
-install.packages("gridExtra") # Download and install gridExtra
-install.packages("hexbin") # Download and install hexbin
 install.packages("tidyverse") # Download and install tidyverse
-install.packages("readr")
-install.packages("dplyr")
-install.packages("magrittr")
-install.packages("tidyr")
-install.packages("knitr")
--- 2021-11/load_packages.R
+++ 2021-11/load_packages.R
@@ -1,13 +1,7 @@
 # Load needed packages into R environment
 
-library(gridExtra) # ggplot
-library(hexbin) # ggplot
-library(tidyverse) # lesson 3 onwards
-library(lubridate)
-library(readr)
-library(ggplot2)
-library(dplyr)
+library(rmarkdown)
+library(tidyverse)
 library(magrittr)
-library(tidyr)
 library(knitr)
 library(methods)
